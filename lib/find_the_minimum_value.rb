ca22bcae2f374f99e5d2ee2c8ebb43b7c1789d43
maxwellbenton <maxwellbenton@gmail.com>
2020-02-29 13:12:07 -0500

    add solution

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,3 +1,11 @@
 def find_min_value(array)
-  # Add your solution here
+  count = 0
+  min_value = 100000000
+  while count < array.length do
+    if min_value > array[count]
+      min_value = array[count]
+    end
+    count += 1
+  end
+  min_value
 end
